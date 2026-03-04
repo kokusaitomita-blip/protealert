9ea50ab4de578d621706858d3ab63efd2e8c4ebf
kokusaitomita-blip <GitHub>
2026-03-04 13:11:20 +0000

    fix nav

--- fix_nav2.py
+++ fix_nav2.py
@@ -1,13 +1,12 @@
 with open(r'C:\protealert\index.html', 'r', encoding='utf-8') as f:
     h = f.read()
-# 2つ目のguideボタンを削除（1つ目だけ残す）
-first = h.find('data-sec="guide"')
-second = h.find('data-sec="guide"', first + 1)
-if second > 0:
-    # 2つ目のbutton全体を削除
-    start = h.rfind('<button', 0, second)
-    end = h.find('</button>', second) + len('</button>')
-    h = h[:start] + h[end:]
+# 全guideボタンを削除
+import re
+h = re.sub(r'\s*<button data-sec="guide">.*?</button>', '', h)
+# 1つだけ追加
+old = '&#x1F4C8; \u4fa1\u683c\u63a8\u79fb</button>\n</nav>'
+new = '&#x1F4C8; \u4fa1\u683c\u63a8\u79fb</button>\n  <button data-sec="guide">\U0001f4d6 \u3042\u308c\u3053\u308c</button>\n</nav>'
+h = h.replace(old, new, 1)
 print('guide count:', h.count('data-sec="guide"'))
 with open(r'C:\protealert\index.html', 'w', encoding='utf-8') as f:
     f.write(h)
